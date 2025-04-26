75e3c80e7f4105ca45e55b803942e4139a0019fa
Yota Toyama <raviqqe@gmail.com>
2025-04-25 18:36:00 -0700

    Fix

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,9 @@
 use crate::{context::Context, error::Error};
 
-pub async fn validate_link(_context: &Context, _link: &str) -> Result<(), Error> {
+pub async fn validate_link(_context: &Context, link: &str) -> Result<(), Error> {
+    let response = reqwest::get(link)
+        .await
+        .map_err(|error| Error::Get(link, error));
+
     Ok(())
 }
