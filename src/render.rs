353b7e05daf99a18848dda7ad0cd60ac4fbe9949
Yota Toyama <raviqqe@gmail.com>
2025-04-25 21:18:47 -0700

    Fix

--- src/render.rs
+++ src/render.rs
@@ -1,5 +1,6 @@
 use crate::context::Context;
 use crate::error::Error;
+use tokio::io::AsyncWriteExt;
 use url::Url;
 
 pub async fn render(
@@ -7,5 +8,23 @@ pub async fn render(
     url: &Url,
     results: &[Result<(), Error>],
 ) -> Result<(), Error> {
+    for result in results {
+        match result {
+            Ok(()) => render_line(format!("{error}", result))?,
+            Err(error) => render_line(error.to_string())?,
+        }
+    }
+
+    Ok(())
+}
+
+async fn render_line(context: &Context, string: &str) -> Result<(), Error> {
+    context
+        .stdout()
+        .lock()
+        .await
+        .write_all(format!("{}\n", string).as_bytes())
+        .await?;
+
     Ok(())
 }
