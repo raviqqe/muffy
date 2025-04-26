f7cab12922ff056c9cbf68e693c46976b79427ae
Yota Toyama <raviqqe@gmail.com>
2025-04-25 18:31:54 -0700

    Fix

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,5 @@
 use crate::{context::Context, error::Error};
 
-pub async fn validate_page(_context: &Context, _url: &str) -> Result<(), Error> {
+pub async fn validate_page(_context: &Context, _link: &str) -> Result<(), Error> {
     Ok(())
 }
