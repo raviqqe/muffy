52b3308b955b841fc1c294a771386c7785fd8838
Yota Toyama <raviqqe@gmail.com>
2025-04-25 18:17:28 -0700

    Fix

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,13 @@
+use core::error;
+use core::fmt::{self, Display, Formatter};
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum Error {}
+
+impl error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Error")
+    }
+}
--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,5 @@
 use crate::error::Error;
 
-pub fn validate_page() -> Result<(), Error> {
+pub async fn validate_page() -> Result<(), Error> {
     Ok(())
 }
