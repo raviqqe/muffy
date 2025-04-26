ce3ddd3894f6df2ad9bd08b5bb2461d77bf6773e
Yota Toyama <raviqqe@gmail.com>
2025-04-25 22:20:17 -0700

    Fix

--- src/cache.rs
+++ src/cache.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
+
 pub struct Cache<T> {
     data: HashMap<String, T>,
+}
+
+impl Cache {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub async get_or_set<F, T>(&mut self, key: String, f: F) -> &T
+    where
+        F: FnOnce() -> T,
+        T: Clone,
+    {
+        if let Some(value) = self.data.get(&key) {
+            value;
+        }else{
+        let value = f();
+        self.data.insert(key.clone(), value.clone());
+        value}
+    }
 }
