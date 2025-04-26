69f3f7e2b6f1ccff92514905c660dcca27f4ee31
Yota Toyama <raviqqe@gmail.com>
2025-04-26 11:11:42 -0700

    Fix

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,5 +1,13 @@
-use crate::{error::Error, response::Response};
+use crate::error::Error;
+use reqwest::{StatusCode, header::HeaderMap};
 use url::Url;
+
+pub struct Response {
+    url: Url,
+    status: StatusCode,
+    headers: HeaderMap,
+    body: Vec<u8>,
+}
 
 pub trait HttpClient {
     fn get(url: &Url) -> impl Future<Output = Result<Response, Error>>;
