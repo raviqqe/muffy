10a1d6e300133c8bb54240e9f5d913897cd79b65
Yota Toyama <raviqqe@gmail.com>
2025-04-25 20:51:38 -0700

    Fix

--- src/context.rs
+++ src/context.rs
@@ -6,13 +6,19 @@ use tokio::{
 #[derive(Debug)]
 pub struct Context {
     stdout: Mutex<Stdout>,
+    origin: String,
 }
 
 impl Context {
-    pub fn new() -> Self {
+    pub fn new(origin: String) -> Self {
         Self {
+            origin,
             stdout: stdout().into(),
         }
+    }
+
+    pub const fn origin(&self) -> &str {
+        &self.origin
     }
 
     pub const fn stdout(&self) -> &Mutex<Stdout> {
