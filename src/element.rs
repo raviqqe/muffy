d60bf26cba25f35955dd27e92d914eec0fa4fe65
Yota Toyama <raviqqe@gmail.com>
2025-04-26 19:22:35 -0700

    Fix

---
+++ src/element.rs
@@ -0,0 +1,18 @@
+pub struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+}
+
+impl Element {
+    pub fn new(name: String, attributes: Vec<(String, Strng)>) -> Self {
+        Self { name, attributes }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn attributes(&self) -> &[(String, String)] {
+        &self.attributes
+    }
+}
