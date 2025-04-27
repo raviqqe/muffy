70b3f55cd0db7c647e71c40fddb42b6cca26bdbe
Yota Toyama <raviqqe@gmail.com>
2025-04-26 17:51:04 -0700

    Fix

--- src/metrics.rs
+++ src/metrics.rs
@@ -5,27 +5,27 @@ pub struct Metrics {
 }
 
 impl Metrics {
-    pub fn new(success: usize, error: usize) -> Self {
-        Metrics { success, error }
+    pub const fn new(success: usize, error: usize) -> Self {
+        Self { success, error }
     }
 
-    pub fn success(&self) -> usize {
+    pub const fn success(&self) -> usize {
         self.success
     }
 
-    pub fn error(&self) -> usize {
+    pub const fn error(&self) -> usize {
         self.error
     }
 
-    pub fn total(&self) -> usize {
+    pub const fn total(&self) -> usize {
         self.success + self.error
     }
 
-    pub fn has_error(&self) -> bool {
+    pub const fn has_error(&self) -> bool {
         self.error > 0
     }
 
-    pub fn add_error(&mut self, error: bool) {
+    pub const fn add_error(&mut self, error: bool) {
         if error {
             self.error += 1;
         } else {
@@ -33,7 +33,7 @@ impl Metrics {
         }
     }
 
-    pub fn merge(&mut self, other: &Self) {
+    pub const fn merge(&mut self, other: &Self) {
         self.success += other.success;
         self.error += other.error;
     }
