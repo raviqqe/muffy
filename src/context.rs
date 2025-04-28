8409eb72691ec60a3bf217502bd1730cb5e52f14
Yota Toyama <raviqqe@gmail.com>
2025-04-27 18:35:58 -0700

    Fix

--- src/context.rs
+++ src/context.rs
@@ -24,7 +24,7 @@ impl Context {
     pub fn new(
         http_client: FullHttpClient,
         job_sender: Sender<Box<dyn Future<Output = Result<Metrics, Error>> + Send>>,
-        robots: Box<dyn Cache<Robots>,
+        robots: Box<dyn Cache<Robots>>,
         origin: String,
     ) -> Self {
         Self {
@@ -33,7 +33,7 @@ impl Context {
             stdout: stdout().into(),
             documents: HashSet::with_capacity(1 << 10),
             job_sender,
-            robots: MemoryCache::new(),
+            robots,
         }
     }
 
@@ -58,5 +58,9 @@ impl Context {
         &self,
     ) -> &Sender<Box<dyn Future<Output = Result<Metrics, Error>> + Send>> {
         &self.job_sender
+    }
+
+    pub fn robots(&self) -> &dyn Cache<Robots> {
+        self.robots.as_ref()
     }
 }
