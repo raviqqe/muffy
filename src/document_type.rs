d829c00597ba3f42ce7bc59ca6815fa4aa7ba917
Yota Toyama <raviqqe@gmail.com>
2025-04-28 21:29:25 -0700

    Fix

--- src/document_type.rs
+++
@@ -1,4 +0,0 @@
-pub enum DocumentType {
-    SitemapXml,
-    Html,
-}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate alloc;
 
 mod cache;
 mod context;
-mod document_type;
 mod element;
 mod error;
 mod full_http_client;
