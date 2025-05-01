93e7aebfadc8cf0d652d9e7a81852b37358e88e4
Yota Toyama <raviqqe@gmail.com>
2025-04-30 21:26:09 -0700

    Fix

--- src/main.rs
+++ src/main.rs
@@ -25,5 +25,63 @@ async fn main() {
 async fn run() -> Result<(), muffy::Error> {
     let Arguments { url, cache } = Arguments::parse();
 
-    muffy::validate(&url, cache).await
+    let mut document_metrics = muffy::Metrics::default();
+    let mut element_metrics = muffy::Metrics::default();
+
+    while let Some(future) = muffy::validate(&url, cache).await {
+        let metrics = Box::into_pin(future).await?;
+
+        document_metrics.add_error(metrics.has_error());
+        element_metrics.merge(&metrics);
+    }
+
+    eprintln!();
+    eprintln!(
+        "{}",
+        Table::from_iter(
+            [vec![
+                "item".into(),
+                "success".into(),
+                "error".into(),
+                "total".into()
+            ]]
+            .into_iter()
+            .chain(
+                [
+                    (
+                        "document",
+                        document_metrics.success(),
+                        document_metrics.error(),
+                        document_metrics.total()
+                    ),
+                    (
+                        "element",
+                        element_metrics.success(),
+                        element_metrics.error(),
+                        element_metrics.total()
+                    )
+                ]
+                .into_iter()
+                .map(|(item, success, error, total)| vec!(
+                    item.to_string(),
+                    success.to_string(),
+                    error.to_string(),
+                    total.to_string()
+                ))
+            )
+        )
+        .with(Style::markdown())
+        .with(Colorization::columns([
+            Color::FG_WHITE,
+            Color::FG_GREEN,
+            Color::FG_RED,
+            Color::FG_WHITE,
+        ])),
+    );
+
+    if document_metrics.has_error() {
+        Err(Error::Document)
+    } else {
+        Ok(())
+    }
 }
