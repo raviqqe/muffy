a8f78df2e3c240f52c3c493a7d6b8ef1125afe07
Yota Toyama <raviqqe@gmail.com>
2025-04-30 21:11:56 -0700

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,10 @@ mod timer;
 mod validation;
 
 use self::cache::{MemoryCache, SledCache};
+pub use self::error::Error;
 use self::metrics::Metrics;
 use self::timer::ClockTimer;
-use self::{context::Context, error::Error, validation::validate_link};
+use self::{context::Context, validation::validate_link};
 use alloc::sync::Arc;
 use dirs::cache_dir;
 use http_client::{CachedHttpClient, ReqwestHttpClient};
@@ -34,7 +35,7 @@ const INITIAL_REQUEST_CACHE_CAPACITY: usize = 1 << 16;
 const JOB_CAPACITY: usize = 1 << 16;
 
 /// Runs validation.
-pub async fn run() -> Result<(), Error> {
+pub async fn validate(url: &str, cache: bool) -> Result<(), Error> {
     let (sender, mut receiver) = channel(JOB_CAPACITY);
     let db = if cache {
         let directory = cache_dir().unwrap_or_else(temp_dir).join("muffy");
@@ -55,10 +56,10 @@ pub async fn run() -> Result<(), Error> {
             (getrlimit(Resource::NOFILE)?.0 / 2) as _,
         ),
         sender,
-        url.clone(),
+        url.into(),
     ));
 
-    validate_link(context, url.clone(), None).await?;
+    validate_link(context, url.into(), None).await?;
 
     let mut document_metrics = Metrics::default();
     let mut element_metrics = Metrics::default();
