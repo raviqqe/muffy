3164c57c53ef8acdf41530fb2077abd12e354e25
Yota Toyama <raviqqe@gmail.com>
2025-05-01 18:51:12 -0700

    Fix

--- src/document.rs
+++ src/document.rs
@@ -1,11 +1,13 @@
 use crate::{Metrics, element::Element, error::Error, success::Success};
 
+/// A document.
 pub struct Document {
     elements: Vec<(Element, Vec<Result<Success, Error>>)>,
     metrics: Metrics,
 }
 
 impl Document {
+    /// Creates a document.
     pub fn new(elements: Vec<(Element, Vec<Result<Success, Error>>)>) -> Self {
         Self {
             metrics: Metrics::new(
@@ -26,10 +28,12 @@ impl Document {
         }
     }
 
+    /// Returns elements with their validation results.
     pub fn elements(&self) -> impl Iterator<Item = &(Element, Vec<Result<Success, Error>>)> {
         self.elements.iter()
     }
 
+    /// Returns metrics of document validation.
     pub fn metrics(&self) -> Metrics {
         self.metrics
     }
