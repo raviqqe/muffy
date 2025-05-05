765c396eeb1a963b7a93ac4815a78b6070f73fa2
Yota Toyama <raviqqe@gmail.com>
2025-05-04 17:45:10 -0700

    Fix

--- src/config.rs
+++ src/config.rs
@@ -9,12 +9,14 @@ pub struct Config {
 }
 
 impl Config {
+    /// Returns websites.
     pub fn sites(&self) -> &HashMap<String, Site> {
         &self.sites
     }
 }
 
 /// A website configuration.
+#[derive(Debug, Deserialize)]
 pub struct Site {
     #[serde(with = "http_serde::header_map")]
     headers: HeaderMap,
