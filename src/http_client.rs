afdd8fb9fc9d45d2a52b1ac2d0b7536682efe266
Yota Toyama <raviqqe@gmail.com>
2025-05-06 19:29:32 -0700

    Fix

--- src/http_client.rs
+++ src/http_client.rs
@@ -6,16 +6,10 @@ mod reqwest;
 mod stub;
 
 #[cfg(test)]
-pub use self::stub::*;
-pub use self::{cached::*, reqwest::*};
-use http::{StatusCode, header::HeaderMap};
-use url::Url;
-
-#[derive(Debug)]
-#[cfg_attr(test, derive(Clone))]
-pub struct BareResponse {
-    pub url: Url,
-    pub status: StatusCode,
-    pub headers: HeaderMap,
-    pub body: Vec<u8>,
-}
+pub use self::stub::StubHttpClient;
+pub use self::{
+    bare::{BareHttpClient, BareResponse},
+    cached::CachedHttpClient,
+    error::HttpClientError,
+    reqwest::ReqwestHttpClient,
+};
