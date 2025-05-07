980d0f224bacedda0e91525593f422138e9a332e
Yota Toyama <raviqqe@gmail.com>
2025-05-06 19:10:50 -0700

    Pass default site config

--- src/config.rs
+++ src/config.rs
@@ -34,23 +34,19 @@ impl Config {
         self.roots.iter().map(Deref::deref)
     }
 
-    /// Returns a default configuration for websites.
-    pub const fn default(&self) -> &SiteConfig {
-        &self.default
-    }
-
     /// Returns websites.
     pub const fn sites(&self) -> &HashMap<String, HostConfig> {
         &self.sites
     }
 
     /// Gets a site config
-    pub fn get_site(&self, url: &Url) -> Option<&SiteConfig> {
+    pub fn site(&self, url: &Url) -> &SiteConfig {
         self.sites()
             .get(url.host_str()?)?
             .get(&url.port().unwrap_or_else(|| default_port(url)))?
             .iter()
             .find_map(|(path, config)| url.path().starts_with(path).then_some(config))
+            .unwrap_or_else(|| self.default())
     }
 }
 
