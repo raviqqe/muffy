6207b3049e13e1c480fd218c0e74ed5397e7d66b
Yota Toyama <raviqqe@gmail.com>
2025-05-08 16:20:23 -0700

    Fix

--- src/http_client/cached_response.rs
+++ src/http_client/cached_response.rs
@@ -1,25 +1,30 @@
 use crate::response::Response;
 use serde::{Deserialize, Serialize};
-use tokio::time::Instant;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CachedResponse {
     response: Response,
-    timestamp: Instant,
+    timestamp: Duration,
 }
 
 impl CachedResponse {
     pub fn new(response: Response) -> Self {
         CachedResponse {
             response,
-            timestamp: Instant::now(),
+            timestamp: Self::now(),
         }
-    }
-    pub fn is_expired(&self, max_age: Duration) -> bool {
-        self.timestamp.elapsed() > max_age
     }
 
     pub fn response(&self) -> &Response {
         &self.response
+    }
+
+    pub fn is_expired(&self, duration: Duration) -> bool {
+        Self::now() - self.timestamp > duration
+    }
+
+    fn now() -> Duration {
+        SystemTime::now().duration_since(UNIX_EPOCH).unwrap()
     }
 }
