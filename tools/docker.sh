5c6a4bc9983c0ea5b03b004e9e8dd88dc498106b
Yota Toyama <raviqqe@gmail.com>
2025-05-07 18:39:45 -0700

    Fix

---
+++ tools/docker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+image=raviqqe/muffy
+version=$(git tag --points-at | sed s/^v//)
+
+docker buildx build \
+  --platform linux/386,linux/amd64,linux/arm,linux/arm64 \
+  --tag $image:latest \
+  ${version:+--tag $image:$version} \
+  "$@" \
+  .
