1747e3cfd1eabc5cfe2b344505bf7ded94dd47b1
Yota Toyama <raviqqe@gmail.com>
2026-01-17 17:13:31 -0800

    fxi

--- src/config/serde.rs
+++ src/config/serde.rs
@@ -14,18 +14,18 @@ pub struct Config {
 /// A site configuration.
 #[derive(Clone, Debug, Default)]
 pub struct SiteConfig {
-    pub headers: HeaderMap,
+    pub headers: Option<HeaderMap>,
     pub status: Option<StatusConfig>,
     pub scheme: Option<SchemeConfig>,
-    pub max_redirects: usize,
-    pub max_age: Duration,
-    pub recursive: bool,
+    pub max_redirects: Option<usize>,
+    pub max_age: Option<Duration>,
+    pub recursive: Option<bool>,
 }
 
 /// A status code configuration.
 #[derive(Clone, Debug)]
 pub struct StatusConfig {
-    accepted: HashSet<StatusCode>,
+    accepted: Option<HashSet<StatusCode>>,
 }
 
 /// A scheme configuration.
