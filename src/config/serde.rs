d61c4d476cc24aef288a8d349e68f82c42629e81
Yota Toyama <raviqqe@gmail.com>
2026-02-16 15:20:55 -0800

    fi

--- src/config/serde.rs
+++ src/config/serde.rs
@@ -60,7 +60,6 @@ struct SiteConfig {
     headers: Option<HashMap<String, String>>,
     ignore: Option<bool>,
     max_redirects: Option<usize>,
-    // TODO Compile rate limiting per site.
     rate_limit: Option<RateLimitConfig>,
     recurse: Option<bool>,
     retry: Option<RetryConfig>,
