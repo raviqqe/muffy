618a86b81fe8ee795ecf341b250452e84009f1cd
Yota Toyama <raviqqe@gmail.com>
2026-02-22 15:32:51 -0800

    Fix

--- muffy/src/render/result.rs
+++ muffy/src/render/result.rs
@@ -1,18 +1,23 @@
-use super::response::RenderedResponse;
-use crate::item_output::ItemOutput;
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct RenderedResult<T, E> {
     error: bool,
     #[serde(flatten)]
-    output: Result<T, E>,
+    result: Result<T, E>,
 }
 
-impl<T, E> RenderedResult<T, E> {
-    pub const fn response(&self) -> Option<&RenderedResponse<'a>> {
-        self.response.as_ref()
+impl<T, E> From<Result<T, E>> for RenderedResult<T, E> {
+    fn from(result: Result<T, E>) -> Self {
+        match result {
+            Ok(value) => Self {
+                error: false,
+                result,
+            },
+            Err(err) => Self {
+                error: true,
+                result,
+            },
+        }
     }
 }
-
-impl<T, E> From<Result<T, E>> for RenderedResult<T, E> {}
