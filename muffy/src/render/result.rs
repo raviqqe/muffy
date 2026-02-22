d3b7f047810bd59c0d47b853b98ad2a94349acb6
Yota Toyama <raviqqe@gmail.com>
2026-02-22 15:33:13 -0800

    Fix

--- muffy/src/render/result.rs
+++ muffy/src/render/result.rs
@@ -9,15 +9,9 @@ pub struct RenderedResult<T, E> {
 
 impl<T, E> From<Result<T, E>> for RenderedResult<T, E> {
     fn from(result: Result<T, E>) -> Self {
-        match result {
-            Ok(value) => Self {
-                error: false,
-                result,
-            },
-            Err(err) => Self {
-                error: true,
-                result,
-            },
+        Self {
+            error: result.is_err(),
+            result,
         }
     }
 }
