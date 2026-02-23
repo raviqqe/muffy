4b112ff003db4bed3ffbb8558a557785ebb61233
Yota Toyama <raviqqe@gmail.com>
2026-02-22 20:29:15 -0800

    Fix

--- rnc/src/lib.rs
+++ rnc/src/lib.rs
@@ -4,25 +4,3 @@ mod ast;
 mod parse;
 
 pub use self::{ast::*, parse::*};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use insta::assert_debug_snapshot;
-    use rstest::rstest;
-    use std::{
-        fs::read_to_string,
-        path::{Path, PathBuf},
-    };
-
-    #[rstest]
-    fn parse_file(#[files("../vendor/validator/**/schema/**/*.rnc")] path: PathBuf) {
-        assert_debug_snapshot!(
-            path.strip_prefix(Path::new("../vendor/validator").canonicalize().unwrap())
-                .unwrap()
-                .display()
-                .to_string(),
-            parse_schema(&read_to_string(&path).unwrap()).unwrap()
-        );
-    }
-}
--- rnc/src/parse.rs
+++ rnc/src/parse.rs
@@ -17,3 +17,25 @@ pub fn parse_schema(input: &str) -> Result<Schema, ParseError> {
     .parse(input)?
     .1)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_debug_snapshot;
+    use rstest::rstest;
+    use std::{
+        fs::read_to_string,
+        path::{Path, PathBuf},
+    };
+
+    #[rstest]
+    fn parse_file(#[files("../vendor/validator/**/schema/**/*.rnc")] path: PathBuf) {
+        assert_debug_snapshot!(
+            path.strip_prefix(Path::new("../vendor/validator").canonicalize().unwrap())
+                .unwrap()
+                .display()
+                .to_string(),
+            parse_schema(&read_to_string(&path).unwrap()).unwrap()
+        );
+    }
+}
