00ae8fefe96e02fd46cb3588a604d76079e3302f
Yota Toyama <raviqqe@gmail.com>
2026-02-22 17:09:12 -0800

    Fix

--- muffy/src/render/result.rs
+++ muffy/src/render/result.rs
@@ -1,31 +1,39 @@
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
-pub struct RenderedResult<T, E> {
-    error: bool,
-    #[serde(flatten)]
-    result: Result<T, E>,
+#[serde(untagged)]
+pub(crate) enum RenderedResult<T, E> {
+    Ok(T),
+    Err(RenderedError<E>),
 }
 
 impl<T, E> RenderedResult<T, E> {
     pub const fn is_ok(&self) -> bool {
-        !self.error
+        matches!(self, Self::Ok(_))
     }
 
     pub const fn is_err(&self) -> bool {
-        self.error
+        matches!(self, Self::Err(_))
     }
 
-    pub const fn result(&self) -> &Result<T, E> {
-        &self.result
+    pub const fn result(&self) -> Result<&T, &E> {
+        match &self {
+            Self::Ok(value) => Ok(value),
+            Self::Err(RenderedError { error }) => Err(error),
+        }
     }
 }
 
 impl<T, E> From<Result<T, E>> for RenderedResult<T, E> {
     fn from(result: Result<T, E>) -> Self {
-        Self {
-            error: result.is_err(),
-            result,
+        match result {
+            Ok(value) => Self::Ok(value),
+            Err(error) => Self::Err(RenderedError { error }),
         }
     }
+}
+
+#[derive(Debug, Serialize)]
+pub(crate) struct RenderedError<E> {
+    error: E,
 }
