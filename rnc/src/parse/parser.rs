4052aa0512f520784d49f3360eb9eb5de2b9a859
Yota Toyama <raviqqe@gmail.com>
2026-02-23 19:08:32 -0800

    Fix

--- rnc/src/parse/parser.rs
+++ rnc/src/parse/parser.rs
@@ -1093,7 +1093,9 @@ mod tests {
 
     #[test]
     fn parse_grammar_with_leading_annotation_block() {
-        let input = "[ sch:pattern [ name = \"select.multiple\" ] ] select = element select { select.attlist, (option | optgroup)+ }";
+        let input = indoc! {r#"
+            [ sch:pattern [ name = "select.multiple" ] ] select = element select { select.attlist, (option | optgroup)+ }
+        "#};
 
         let (remaining_input, _) = grammar(input).unwrap();
 
