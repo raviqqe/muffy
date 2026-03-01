3d5974ab058110d5c642643a7abc88f112d3e500
Yota Toyama <raviqqe@gmail.com>
2026-02-28 17:15:42 -0800

    Fix

---
+++ document/src/html_parser.rs
@@ -0,0 +1,244 @@
+mod node;
+
+pub use self::node::Node;
+use crate::{
+    cache::{Cache, CacheError},
+    response::Response,
+};
+use alloc::sync::Arc;
+use core::{
+    error::Error,
+    fmt,
+    fmt::{Display, Formatter},
+};
+use html5ever::{parse_document, tendril::TendrilSink};
+use markup5ever_rcdom::RcDom;
+use node::Document;
+use std::io;
+
+/// An HTML parser.
+pub struct HtmlParser {
+    cache: Box<dyn Cache<Result<Arc<Document>, HtmlError>>>,
+}
+
+impl HtmlParser {
+    /// Creates an HTML parser.
+    pub fn new(cache: impl Cache<Result<Arc<Document>, HtmlError>> + 'static) -> Self {
+        Self {
+            cache: Box::new(cache),
+        }
+    }
+
+    /// Parses an HTML document.
+    pub async fn parse(&self, response: &Arc<Response>) -> Result<Arc<Document>, HtmlError> {
+        let response = response.clone();
+
+        self.cache
+            .get_with(
+                response.url().to_string(),
+                Box::new(async move {
+                    parse_document(RcDom::default(), Default::default())
+                        .from_utf8()
+                        .read_from(&mut response.body())
+                        .map(|dom| Document::from_markup5ever(&dom.document).into())
+                        .map_err(|error| HtmlError::Io(error.into()))
+                }),
+            )
+            .await?
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum HtmlError {
+    Cache(CacheError),
+    Io(Arc<io::Error>),
+}
+
+impl Error for HtmlError {}
+
+impl Display for HtmlError {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Cache(error) => write!(formatter, "{error}"),
+            Self::Io(error) => write!(formatter, "{error}"),
+        }
+    }
+}
+
+impl From<CacheError> for HtmlError {
+    fn from(error: CacheError) -> Self {
+        Self::Cache(error)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{MemoryCache, html_parser::node::Element};
+    use http::StatusCode;
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+    use url::Url;
+
+    #[tokio::test]
+    async fn parse_response() {
+        let parser = HtmlParser::new(MemoryCache::new(0));
+
+        assert_eq!(
+            parser
+                .parse(&Arc::new(Response::new(
+                    Url::parse("https://foo.com").unwrap(),
+                    StatusCode::OK,
+                    Default::default(),
+                    r#"<a href="https://foo.com/bar"></a>"#.trim().as_bytes().to_vec(),
+                    Default::default(),
+                )))
+                .await
+                .unwrap(),
+            Document::new(vec![Arc::new(
+                Element::new(
+                    "html".into(),
+                    vec![],
+                    vec![
+                        Arc::new(Element::new("head".into(), vec![], vec![]).into()),
+                        Arc::new(
+                            Element::new(
+                                "body".into(),
+                                vec![],
+                                vec![Arc::new(
+                                    Element::new(
+                                        "a".into(),
+                                        vec![("href".into(), "https://foo.com/bar".into())],
+                                        vec![]
+                                    )
+                                    .into()
+                                )]
+                            )
+                            .into()
+                        )
+                    ]
+                )
+                .into()
+            )])
+            .into()
+        );
+    }
+
+    #[tokio::test]
+    async fn parse_base() {
+        let parser = HtmlParser::new(MemoryCache::new(0));
+
+        assert_eq!(
+            parser
+                .parse(&Arc::new(Response::new(
+                    Url::parse("https://foo.com").unwrap(),
+                    StatusCode::OK,
+                    Default::default(),
+                    indoc! {r#"
+                        <html>
+                            <head>
+                                <base href="https://foo.com/foo/" />
+                            </head>
+                        </html>
+                    "#}
+                    .trim()
+                    .as_bytes()
+                    .to_vec(),
+                    Default::default(),
+                )))
+                .await
+                .unwrap()
+                .base(),
+            Some("https://foo.com/foo/")
+        );
+    }
+
+    #[tokio::test]
+    async fn parse_base_without_href() {
+        let parser = HtmlParser::new(MemoryCache::new(0));
+
+        assert_eq!(
+            parser
+                .parse(&Arc::new(Response::new(
+                    Url::parse("https://foo.com").unwrap(),
+                    StatusCode::OK,
+                    Default::default(),
+                    indoc! {r#"
+                        <html>
+                            <head>
+                                <base target="_blank" />
+                            </head>
+                        </html>
+                    "#}
+                    .trim()
+                    .as_bytes()
+                    .to_vec(),
+                    Default::default(),
+                )))
+                .await
+                .unwrap()
+                .base(),
+            None
+        );
+    }
+
+    #[tokio::test]
+    async fn parse_multiple_base_elements() {
+        let parser = HtmlParser::new(MemoryCache::new(0));
+
+        assert_eq!(
+            parser
+                .parse(&Arc::new(Response::new(
+                    Url::parse("https://foo.com").unwrap(),
+                    StatusCode::OK,
+                    Default::default(),
+                    indoc! {r#"
+                        <html>
+                            <head>
+                                <base href="https://foo.com/first/" />
+                                <base href="https://foo.com/second/" />
+                            </head>
+                        </html>
+                    "#}
+                    .trim()
+                    .as_bytes()
+                    .to_vec(),
+                    Default::default(),
+                )))
+                .await
+                .unwrap()
+                .base(),
+            Some("https://foo.com/first/")
+        );
+    }
+
+    #[tokio::test]
+    async fn parse_base_in_body() {
+        let parser = HtmlParser::new(MemoryCache::new(0));
+
+        assert_eq!(
+            parser
+                .parse(&Arc::new(Response::new(
+                    Url::parse("https://foo.com").unwrap(),
+                    StatusCode::OK,
+                    Default::default(),
+                    indoc! {r#"
+                        <html>
+                            <head></head>
+                            <body>
+                                <base href="https://foo.com/foo/" />
+                            </body>
+                        </html>
+                    "#}
+                    .trim()
+                    .as_bytes()
+                    .to_vec(),
+                    Default::default(),
+                )))
+                .await
+                .unwrap()
+                .base(),
+            Some("https://foo.com/foo/")
+        );
+    }
+}
