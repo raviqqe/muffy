507ac14b86bccb72fc234a9016e04a9c3f61883a
Yota Toyama <raviqqe@gmail.com>
2026-02-28 22:02:52 -0800

    Fix

--- muffy-validation-macro/src/lib.rs
+++ muffy-validation-macro/src/lib.rs
@@ -5,10 +5,10 @@ use quote::quote;
 
 /// Generates HTML validation functions.
 #[proc_macro]
-pub fn validation(input: TokenStream) -> TokenStream {
+pub fn html(_input: TokenStream) -> TokenStream {
     // TODO Generate validation functions of `validate_element` for HTML against the `Element` type
     // in the `muffy-document` crate based on the HTML specification in the Relax NG Compact syntax
     // at the `schema` directory using the parser in the `muffy-rnc` crate.
 
-    quote! {}
+    quote! {}.into()
 }
