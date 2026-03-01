dda8614c7dd164b41bdf6e25b18c7c3da18e5302
Yota Toyama <raviqqe@gmail.com>
2026-02-28 22:20:45 -0800

    HTML validation macro (#735)

---
+++ validation-macro/src/lib.rs
@@ -0,0 +1,14 @@
+//! Macros for document validation.
+
+use proc_macro::TokenStream;
+use quote::quote;
+
+/// Generates HTML validation functions.
+#[proc_macro]
+pub fn html(_input: TokenStream) -> TokenStream {
+    // TODO Generate validation functions of `validate_element` for HTML against the `Element` type
+    // in the `muffy-document` crate based on the HTML specification in the Relax NG Compact syntax
+    // at the `schema` directory using the parser in the `muffy-rnc` crate.
+
+    quote! {}.into()
+}
