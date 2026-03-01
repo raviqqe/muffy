cddb3f7a99a7ecbf7aee9a08c9e24c336e940d37
Yota Toyama <raviqqe@gmail.com>
2026-02-28 17:34:08 -0800

    fix

--- document/src/html.rs
+++ document/src/html.rs
@@ -1,17 +1,4 @@
 mod node;
+mod parse;
 
-pub use self::node::Node;
-use html5ever::{parse_document, tendril::TendrilSink};
-use markup5ever_rcdom::RcDom;
-use node::Document;
-use std::io;
-
-/// Parses an HTML document.
-pub fn parse(source: &str) -> Result<Document, io::Error> {
-    let mut source = source.as_bytes();
-
-    parse_document(RcDom::default(), Default::default())
-        .from_utf8()
-        .read_from(&mut source)
-        .map(|dom| Document::from_markup5ever(&dom.document).into())
-}
+pub use self::parse::*;
---
+++ document/src/html/parse.rs
@@ -0,0 +1,15 @@
+pub use super::node::Node;
+use html5ever::{parse_document, tendril::TendrilSink};
+use markup5ever_rcdom::RcDom;
+use node::Document;
+use std::io;
+
+/// Parses an HTML document.
+pub fn parse(source: &str) -> Result<Document, io::Error> {
+    let mut source = source.as_bytes();
+
+    parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(&mut source)
+        .map(|dom| Document::from_markup5ever(&dom.document))
+}
