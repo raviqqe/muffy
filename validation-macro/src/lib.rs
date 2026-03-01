9755d051737053648329bad40dc080757d1bccf4
Yota Toyama <raviqqe@gmail.com>
2026-02-28 23:03:29 -0800

    Validation crate (#736)
    - **Fix**
    - **Fix**

--- validation-macro/src/lib.rs
+++ validation-macro/src/lib.rs
@@ -6,9 +6,10 @@ use quote::quote;
 /// Generates HTML validation functions.
 #[proc_macro]
 pub fn html(_input: TokenStream) -> TokenStream {
-    // TODO Generate validation functions of `validate_element` for HTML against the `Element` type
-    // in the `muffy-document` crate based on the HTML specification in the Relax NG Compact syntax
-    // at the `schema` directory using the parser in the `muffy-rnc` crate.
+    // TODO Generate validation functions of `validate_element` for HTML against the
+    // `Element` type in the `muffy-document` crate based on the HTML
+    // specification in the Relax NG Compact syntax at the `schema` directory
+    // using the parser in the `muffy-rnc` crate.
 
     quote! {}.into()
 }
