9c86c42934cd2496311f9be50468c09cf270d2f0
Yota Toyama <raviqqe@gmail.com>
2026-02-28 22:01:00 -0800

    Fix

--- muffy-validation-macro/src/lib.rs
+++ muffy-validation-macro/src/lib.rs
@@ -1,10 +1,11 @@
 //! Macros for document validation.
 
 use proc_macro::TokenStream;
+use quote::quote;
 
 /// Generates HTML validation functions.
 #[proc_macro]
-pub fn validation(input: &TokenStream) -> TokenStream {
+pub fn validation(input: TokenStream) -> TokenStream {
     // TODO Generate validation functions of `validate_element` for HTML against the `Element` type
     // in the `muffy-document` crate based on the HTML specification in the Relax NG Compact syntax
     // at the `schema` directory using the parser in the `muffy-rnc` crate.
