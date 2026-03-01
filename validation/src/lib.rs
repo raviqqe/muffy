a15d109a83c46f029e42a9c37b1fd07f8e4f5357
Yota Toyama <raviqqe@gmail.com>
2026-03-01 14:46:10 -0800

    Fix

--- validation/src/lib.rs
+++ validation/src/lib.rs
@@ -22,78 +22,258 @@ mod tests {
     use muffy_document::html::Node;
     use std::sync::Arc;
 
-    #[test]
-    fn validate_valid_html_element() {
-        let element = Element::new("html".to_owned(), vec![], vec![]);
-
-        assert_eq!(validate_element(&element), Ok(()));
+    fn create_element(
+        name: &str,
+        attributes: Vec<(&str, &str)>,
+        children: Vec<Element>,
+    ) -> Element {
+        Element::new(
+            name.to_owned(),
+            attributes
+                .into_iter()
+                .map(|(k, v)| (k.to_owned(), v.to_owned()))
+                .collect(),
+            children
+                .into_iter()
+                .map(|e| Arc::new(Node::Element(e)))
+                .collect(),
+        )
     }
 
     #[test]
     fn validate_invalid_element_name() {
-        let element = Element::new("invalid".to_owned(), vec![], vec![]);
-
+        let element = create_element("invalid", vec![], vec![]);
         assert_eq!(
             validate_element(&element),
             Err(ValidationError::InvalidElement("invalid".to_owned()))
         );
     }
 
-    #[test]
-    fn validate_valid_attribute() {
-        let element = Element::new(
-            "div".to_owned(),
-            vec![("id".to_owned(), "foo".to_owned())],
-            vec![],
-        );
+    mod div {
+        use super::*;
 
-        assert_eq!(validate_element(&element), Ok(()));
+        #[test]
+        fn validate_valid_element() {
+            let element = create_element("div", vec![], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_valid_attributes() {
+            let element = create_element("div", vec![("id", "foo"), ("class", "bar")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_invalid_attribute() {
+            let element = create_element("div", vec![("invalid", "foo")], vec![]);
+            assert_eq!(
+                validate_element(&element),
+                Err(ValidationError::InvalidAttribute("invalid".to_owned()))
+            );
+        }
+
+        #[test]
+        fn validate_valid_child() {
+            let element = create_element("div", vec![], vec![create_element("p", vec![], vec![])]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
     }
 
-    #[test]
-    fn validate_invalid_attribute() {
-        let element = Element::new(
-            "div".to_owned(),
-            vec![("invalid".to_owned(), "bar".to_owned())],
-            vec![],
-        );
+    mod p {
+        use super::*;
 
-        assert_eq!(
-            validate_element(&element),
-            Err(ValidationError::InvalidAttribute("invalid".to_owned()))
-        );
+        #[test]
+        fn validate_valid_element() {
+            let element = create_element("p", vec![], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_invalid_child() {
+            let element = create_element("p", vec![], vec![create_element("div", vec![], vec![])]);
+            assert_eq!(
+                validate_element(&element),
+                Err(ValidationError::InvalidChild("div".to_owned()))
+            );
+        }
     }
 
-    #[test]
-    fn validate_valid_child() {
-        let element = Element::new(
-            "div".to_owned(),
-            vec![],
-            vec![Arc::new(Node::Element(Element::new(
-                "p".to_owned(),
-                vec![],
-                vec![],
-            )))],
-        );
+    mod html {
+        use super::*;
 
-        assert_eq!(validate_element(&element), Ok(()));
+        #[test]
+        fn validate_valid_element() {
+            let element = create_element("html", vec![], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_valid_children() {
+            let element = create_element(
+                "html",
+                vec![],
+                vec![
+                    create_element("head", vec![], vec![]),
+                    create_element("body", vec![], vec![]),
+                ],
+            );
+            assert_eq!(validate_element(&element), Ok(()));
+        }
     }
 
-    #[test]
-    fn validate_invalid_child() {
-        let element = Element::new(
-            "p".to_owned(),
-            vec![],
-            vec![Arc::new(Node::Element(Element::new(
-                "div".to_owned(),
-                vec![],
-                vec![],
-            )))],
-        );
+    mod head {
+        use super::*;
 
-        assert_eq!(
-            validate_element(&element),
-            Err(ValidationError::InvalidChild("div".to_owned()))
-        );
+        #[test]
+        fn validate_valid_child() {
+            let element = create_element("head", vec![], vec![create_element("title", vec![], vec![])]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_invalid_child() {
+            let element = create_element("head", vec![], vec![create_element("p", vec![], vec![])]);
+            assert_eq!(
+                validate_element(&element),
+                Err(ValidationError::InvalidChild("p".to_owned()))
+            );
+        }
+    }
+
+    mod title {
+        use super::*;
+
+        #[test]
+        fn validate_invalid_child() {
+            let element = create_element("title", vec![], vec![create_element("div", vec![], vec![])]);
+            assert_eq!(
+                validate_element(&element),
+                Err(ValidationError::InvalidChild("div".to_owned()))
+            );
+        }
+    }
+
+    mod ul {
+        use super::*;
+
+        #[test]
+        fn validate_valid_child() {
+            let element = create_element("ul", vec![], vec![create_element("li", vec![], vec![])]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_invalid_child() {
+            let element = create_element("ul", vec![], vec![create_element("p", vec![], vec![])]);
+            assert_eq!(
+                validate_element(&element),
+                Err(ValidationError::InvalidChild("p".to_owned()))
+            );
+        }
+    }
+
+    mod table {
+        use super::*;
+
+        #[test]
+        fn validate_valid_child() {
+            let element = create_element("table", vec![], vec![create_element("tr", vec![], vec![])]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_invalid_child() {
+            let element = create_element("table", vec![], vec![create_element("p", vec![], vec![])]);
+            assert_eq!(
+                validate_element(&element),
+                Err(ValidationError::InvalidChild("p".to_owned()))
+            );
+        }
+    }
+
+    mod tr {
+        use super::*;
+
+        #[test]
+        fn validate_valid_children() {
+            let element = create_element(
+                "tr",
+                vec![],
+                vec![
+                    create_element("th", vec![], vec![]),
+                    create_element("td", vec![], vec![]),
+                ],
+            );
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+    }
+
+    mod form {
+        use super::*;
+
+        #[test]
+        fn validate_valid_attributes() {
+            let element = create_element("form", vec![("action", "/"), ("method", "post")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_valid_child() {
+            let element = create_element("form", vec![], vec![create_element("input", vec![], vec![])]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+    }
+
+    mod img {
+        use super::*;
+
+        #[test]
+        fn validate_valid_attributes() {
+            let element = create_element("img", vec![("src", "img.png"), ("alt", "description")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+    }
+
+    mod video {
+        use super::*;
+
+        #[test]
+        fn validate_valid_attributes() {
+            let element = create_element("video", vec![("src", "vid.mp4"), ("controls", "")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_valid_child() {
+            let element = create_element("video", vec![], vec![create_element("track", vec![], vec![])]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+    }
+
+    mod meta {
+        use super::*;
+
+        #[test]
+        fn validate_valid_name_content() {
+            let element = create_element("meta", vec![("name", "description"), ("content", "stuff")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+
+        #[test]
+        fn validate_valid_charset() {
+            let element = create_element("meta", vec![("charset", "utf-8")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
+    }
+
+    mod link {
+        use super::*;
+
+        #[test]
+        fn validate_valid_attributes() {
+            let element = create_element("link", vec![("rel", "stylesheet"), ("href", "style.css")], vec![]);
+            assert_eq!(validate_element(&element), Ok(()));
+        }
     }
 }
