5efed46cbf5e96dfa3d9224a2c5dfe3f2831efdf
Yota Toyama <raviqqe@gmail.com>
2026-02-28 17:52:15 -0800

    fix

--- document/src/html/parse.rs
+++ document/src/html/parse.rs
@@ -5,11 +5,9 @@ use std::io;
 
 /// Parses an HTML document.
 pub fn parse(source: &str) -> Result<Document, io::Error> {
-    let mut source = source.as_bytes();
-
     parse_document(RcDom::default(), Default::default())
         .from_utf8()
-        .read_from(&mut source)
+        .read_from(&mut source.as_bytes())
         .map(|dom| Document::from_markup5ever(&dom.document))
 }
 
