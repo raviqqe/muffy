437df985551fff4c20e48cc4436e12d47ee7e4bd
Yota Toyama <raviqqe@gmail.com>
2026-03-04 20:23:01 -0800

    WIP

--- validation-macro/src/lib.rs
+++ validation-macro/src/lib.rs
@@ -69,7 +69,10 @@ fn generate_html() -> Result<TokenStream, MacroError> {
 
         element_matches.push(quote! {
             #element => {
-                let mut attributes = ::alloc::collections::BTreeMap::default();
+                let mut attributes: ::alloc::collections::BTreeMap<
+                    String,
+                    ::alloc::collections::BTreeSet<AttributeError>,
+                > = Default::default();
 
                 for (attribute_name, _) in element.attributes() {
                     match attribute_name {
@@ -84,7 +87,10 @@ fn generate_html() -> Result<TokenStream, MacroError> {
                     }
                 }
 
-                let mut children = ::alloc::collections::BTreeMap::default();
+                let mut children: ::alloc::collections::BTreeMap<
+                    String,
+                    ::alloc::collections::BTreeSet<ChildError>,
+                > = Default::default();
 
                 for child in element.children() {
                     if let muffy_document::html::Node::Element(child_element) = child {
