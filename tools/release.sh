6ef5073d75f22799c11d1036987f0093687a5e81
Yota Toyama <raviqqe@gmail.com>
2026-03-07 16:29:40 -0800

    Fix

---
+++ tools/release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ex
+
+cargo build
+
+git add -f validation-macro/src/schema
+
+git config user.email action@github.com
+git config user.name 'GitHub Action'
+git commit -m release
+
+cargo install cargo-workspaces
+cargo workspaces publish -y --from-git "$@"
