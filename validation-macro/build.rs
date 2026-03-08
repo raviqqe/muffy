7f42af83fbc9ec06a5f6f6a42eeeaa762998286a
Yota Toyama <raviqqe@gmail.com>
2026-03-07 16:23:59 -0800

    Fix schema directory again (#774)
    - **Fix schema building again**
    - **Fix**

--- validation-macro/build.rs
+++ validation-macro/build.rs
@@ -1,20 +1,22 @@
 //! A build script.
 
 use core::error::Error;
-use std::{path::Path, process::Command};
+use std::{fs::exists, path::Path, process::Command};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    Command::new("ln")
-        .arg("-s")
-        .arg(
-            Path::new("..")
-                .join("..")
-                .join("vendor")
-                .join("validator")
-                .join("schema"),
-        )
-        .arg("src")
-        .output()?;
+    if !exists(Path::new("src").join("schema"))? {
+        Command::new("ln")
+            .arg("-s")
+            .arg(
+                Path::new("..")
+                    .join("..")
+                    .join("vendor")
+                    .join("validator")
+                    .join("schema"),
+            )
+            .arg("src")
+            .output()?;
+    }
 
     Ok(())
 }
