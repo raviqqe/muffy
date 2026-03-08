3ff16c98fbce0e656849d2d326d454887dc47f1f
Yota Toyama <raviqqe@gmail.com>
2026-03-07 16:38:21 -0800

    Fix release workflow (#775)
    - **Fix**
    - **Fix**
    - **Fix**

---
+++ tools/release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+[ -n "$CI" ]
+
+# Build asset files ahead of release.
+cargo build
+git add -f validation-macro/src/schema
+
+git config user.email action@github.com
+git config user.name 'GitHub Action'
+git commit -m release
+
+cargo install cargo-workspaces
+cargo workspaces publish -y --from-git "$@"
