e81ff8be078b146059e9b59f983062ff64868a7d
Yota Toyama <raviqqe@gmail.com>
2026-07-25 16:46:17 -0700

    Remove security headers on redirects (#1141)

--- muffy/src/request.rs
+++ muffy/src/request.rs
@@ -1,7 +1,10 @@
 use crate::{RetryConfig, http_client::BareRequest};
 use alloc::sync::Arc;
 use core::time::Duration;
-use http::HeaderMap;
+use http::{
+    HeaderMap,
+    header::{AUTHORIZATION, COOKIE, PROXY_AUTHORIZATION},
+};
 use url::Url;
 
 #[derive(Clone, Debug)]
@@ -93,5 +96,87 @@ impl Request {
     pub fn set_url(mut self, url: Url) -> Self {
         self.bare.url = url;
         self
+    }
+
+    pub fn redirect(mut self, url: Url) -> Self {
+        if url.origin() != self.bare.url.origin() {
+            for name in [AUTHORIZATION, COOKIE, PROXY_AUTHORIZATION] {
+                self.bare.headers.remove(name);
+            }
+        }
+
+        self.set_url(url)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use http::{HeaderValue, header::ACCEPT};
+    use pretty_assertions::assert_eq;
+
+    fn credentialed_request(url: &str) -> Request {
+        Request::new(
+            Url::parse(url).unwrap(),
+            [
+                (AUTHORIZATION, HeaderValue::from_static("secret")),
+                (COOKIE, HeaderValue::from_static("session=abc")),
+                (ACCEPT, HeaderValue::from_static("text/html")),
+            ]
+            .into_iter()
+            .collect(),
+        )
+    }
+
+    #[test]
+    fn strip_credentials_on_cross_origin_redirect() {
+        let request = credentialed_request("https://foo.com/page")
+            .redirect(Url::parse("https://bar.com/page").unwrap());
+
+        assert_eq!(request.url().as_str(), "https://bar.com/page");
+        assert_eq!(request.as_bare().headers.get(AUTHORIZATION), None);
+        assert_eq!(request.as_bare().headers.get(COOKIE), None);
+    }
+
+    #[test]
+    fn keep_credentials_on_same_origin_redirect() {
+        let request = credentialed_request("https://foo.com/page")
+            .redirect(Url::parse("https://foo.com/other").unwrap());
+
+        assert_eq!(
+            request.as_bare().headers.get(AUTHORIZATION),
+            Some(&HeaderValue::from_static("secret"))
+        );
+        assert_eq!(
+            request.as_bare().headers.get(COOKIE),
+            Some(&HeaderValue::from_static("session=abc"))
+        );
+    }
+
+    #[test]
+    fn strip_credentials_on_scheme_downgrade() {
+        let request = credentialed_request("https://foo.com/page")
+            .redirect(Url::parse("http://foo.com/page").unwrap());
+
+        assert_eq!(request.as_bare().headers.get(AUTHORIZATION), None);
+    }
+
+    #[test]
+    fn strip_credentials_on_port_change() {
+        let request = credentialed_request("https://foo.com/page")
+            .redirect(Url::parse("https://foo.com:8443/page").unwrap());
+
+        assert_eq!(request.as_bare().headers.get(AUTHORIZATION), None);
+    }
+
+    #[test]
+    fn keep_other_header_on_cross_origin_redirect() {
+        let request = credentialed_request("https://foo.com/page")
+            .redirect(Url::parse("https://bar.com/page").unwrap());
+
+        assert_eq!(
+            request.as_bare().headers.get(ACCEPT),
+            Some(&HeaderValue::from_static("text/html"))
+        );
     }
 }
