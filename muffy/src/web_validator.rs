d775edab46948a5cd6f71363f61fa36dd62cc5f1
Yota Toyama <raviqqe@gmail.com>
2026-08-02 14:50:34 -0700

    Refactor

--- muffy/src/web_validator.rs
+++ muffy/src/web_validator.rs
@@ -20,7 +20,7 @@ use alloc::sync::Arc;
 use core::{iter, str};
 use futures::{Stream, StreamExt, future::try_join_all};
 use itertools::Itertools;
-use muffy_document::html::Node;
+use muffy_document::html::{self, Node};
 use muffy_validation::MarkupError;
 use std::collections::HashMap;
 use tokio::{spawn, sync::mpsc::channel, task::JoinHandle};
@@ -286,7 +286,7 @@ impl WebValidator {
         &self,
         context: &Arc<Context>,
         base: &Arc<Url>,
-        element: &muffy_document::html::Element,
+        element: &html::Element,
     ) -> Option<ElementFuture> {
         let attributes = HashMap::<_, _>::from_iter(element.attributes());
         let mut links = vec![];
@@ -697,7 +697,7 @@ impl WebValidator {
     }
 
     fn create_output_element<'a>(
-        element: &muffy_document::html::Element,
+        element: &html::Element,
         attributes: &HashMap<&str, &str>,
         link_attributes: impl IntoIterator<Item = &'a str>,
         validation_result: &'a Result<(), MarkupError>,
