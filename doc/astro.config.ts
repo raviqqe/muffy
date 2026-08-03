678f25542d2aa27c4f1e0cd398a61653f833b257
Yota Toyama <raviqqe@gmail.com>
2026-08-02 18:59:37 -0700

    Update readme (#1178)

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -47,10 +47,6 @@ export default defineConfig({
               label: "GitHub Action",
               link: "/usage/github-action",
             },
-            {
-              label: "Docker",
-              link: "/usage/docker",
-            },
           ],
           label: "Usage",
         },
