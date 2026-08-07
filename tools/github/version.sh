40e4d2a523c8cd421caed5bae340ff2f3ee5b1c3
Yota Toyama <raviqqe@gmail.com>
2026-08-06 19:20:35 -0700

    Fix

--- tools/github/version.sh
+++ tools/github/version.sh
@@ -5,4 +5,4 @@ set -e
 version=$(yq .workspace.package.version Cargo.toml)
 
 echo version=$version >>$GITHUB_OUTPUT
-echo cache-version=${version%.*} >>$GITHUB_OUTPUT
+echo cache_version=${version%.*} >>$GITHUB_OUTPUT
