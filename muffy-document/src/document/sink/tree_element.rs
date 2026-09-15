52ee7de07495d8614ed0993c2c2af7863dc7bbde
Yota Toyama <raviqqe@gmail.com>
2026-09-14 21:52:11 -0700

    Typed arena while document building (#1348)

--- muffy-document/src/document/sink/tree_element.rs
+++ muffy-document/src/document/sink/tree_element.rs
@@ -1,8 +1,10 @@
+use super::tree_node::TreeNode;
+use core::cell::RefCell;
 use markup5ever::{Attribute, QualName};
 
-pub struct TreeElement {
+pub struct TreeElement<'a> {
     pub name: QualName,
-    pub attributes: Vec<Attribute>,
-    pub template_contents: Option<usize>,
+    pub attributes: RefCell<Vec<Attribute>>,
+    pub template_contents: Option<&'a TreeNode<'a>>,
     pub mathml_annotation_xml_integration_point: bool,
 }
